55ea90724e7696e3c5b8cf9dea3d0f198d65f7fc
bmac <bmac325@gmail.com>
2013-01-19 15:42:55 -0500

    remove redirecting output to log file

--- post_wpi_daka.sh
+++ post_wpi_daka.sh
@@ -5,7 +5,7 @@ source ~/wpidakasecrets.txt
 
 # run the script using the virtual env's python and log any errors
 
-echo "Started running wpidaka.py at `date`" &>> ~/wpidakalog.txt
-~/virtualenv/wpidaka/bin/python ~/code/wpidaka/wpidaka.py -m $1 -v &>> ~/wpidakalog.txt
-echo "Completed wpidaka.py at `date`" &>> ~/wpidakalog.txt
+echo "Started running wpidaka.py at `date`"
+~/virtualenv/wpidaka/bin/python ~/code/wpidaka/wpidaka.py -m $1 -v
+echo "Completed wpidaka.py at `date`"
 
